9c5e081ae81cc2489be2e412d873e945461aa7ff
Paul-202425 <p.masamvu@alustudent.com>
2025-09-15 22:36:31 +0200

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
 import json
+
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,8 +13,9 @@ from models.review import Review
 
 class FileStorage:
     """Represent an abstracted storage engine.
+
     Attributes:
-        __file_path (str): The name of the file to save objects to.
+        __file_path (str): The file path used to store JSON data.
         __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
@@ -24,14 +26,15 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
+        """Set in __objects obj with key <obj_class_name>.id."""
         ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        key = "{}.{}".format(ocname, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        objdict = {k: v.to_dict() for k, v in odict.items()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
 
@@ -40,9 +43,9 @@ class FileStorage:
         try:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+            for o in objdict.values():
+                cls_name = o["__class__"]
+                del o["__class__"]
+                self.new(eval(cls_name)(**o))
         except FileNotFoundError:
-            return
+            pass
