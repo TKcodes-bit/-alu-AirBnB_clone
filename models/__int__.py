fe1056f84991403886ac27d045d71d42ab4ca898
TKcodes-bit <t.odongo@alustudents.com>
2025-09-16 11:07:14 +0300

    __init__

--- models/__int__.py
+++ models/__int__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
+"""Initialize the models package and create a unique FileStorage instance."""
+
 from models.engine.file_storage import FileStorage
 
-
+# Create storage instance
 storage = FileStorage()
+# Reload objects from file.json, if it exists
 storage.reload()
+
